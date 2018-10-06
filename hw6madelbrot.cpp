b1c6dddd296311427fc7bf6549a9578098f237cc
Ember Fairbanks <ember.fairbanks@aggiemail.usu.edu>
2018-10-05 20:06:02 -0600

    Add finished hw6

--- hw6madelbrot.cpp
+++ hw6madelbrot.cpp
@@ -15,10 +15,8 @@ void getColor(int i, vector<int> &rgbVals){
 	r=(log(i)/log(1024))*255;
 	g=(log(i)/log(1024))*55;
 	b=(log(i)/log(1024))*255;
+
 	if (i==1024) {r=0; g=0; b=0;}
-	//cout << r << " ";
-	//cout << g << " ";
-	//cout << b << " ";															    
 	rgbVals.push_back(r);
 	rgbVals.push_back(g);
 	rgbVals.push_back(b);
@@ -69,6 +67,8 @@ int main(int argc, char **argv){
 	int rank, size;
 	int currentRow;
 	vector<int> rgbVect;
+	int mainArray[PIXELS][PIXELS * 3]; //2d array with PIXELS rows and PIXELS*3 columns.
+	//PIXELS*3 columns are needed to hold an R, G, and B value for each pixel.
   c1.r = -2.0;
   c2.r = 0.0;
   c1.i = 1.0;
@@ -81,72 +81,73 @@ int main(int argc, char **argv){
 	MPI_Comm_rank(MCW, &rank);
 	MPI_Comm_size(MCW, &size);
 	
-	if(rank == 0) {
-	  cout << "P3" <<endl;
+	if(rank == 0) { //I am the master
+	  cout << "P3" <<endl; //Get PPM file ready
 		cout << PIXELS << " " << PIXELS << endl;
 	  cout << "255" <<endl;
-		int currentProcess = 1;
-//		currentRow = 0;
-//		while(currentRow < PIXELS) {
-//			cout<<"%%%%%%%%%%%%%%  "<<currentRow<<"  %%%%%%%%%%%%%%"<<endl<<endl;
-//			for(int i=1;i<size;++i){
-//				MPI_Send(&currentRow, 1, MPI_INT, i, 0, MCW); 
-//				currentRow++;
-//			}
-//			currentRow-=size-1;
-//			for(int j=1;j<size;++j) {
-//				cout<<"current row = "<<currentRow<<" rank: "<<j<<endl;
-//				rgbVect.resize(PIXELS*3);
-//				MPI_Recv(&rgbVect[0], (PIXELS * 3), MPI_INT, j, currentRow, MCW, MPI_STATUS_IGNORE);
-//				currentRow++;
-//				int index = 0;
-//				for(int j = 0; j < PIXELS; ++j) {
-//					for(int k = 0; k < 3; k++) {
-//						cout<<rgbVect[index]<<" ";
-//						index++;
-//					}
-//				}
-//				cout<<endl;
-//			}
-//		}
-			currentProcess = 1;
-			for(int i=0;i<PIXELS;++i){
-				currentRow = i;
-			cout<<"current row in for loop: "<<currentRow<<" Process: "<<currentProcess<<endl;	
-				MPI_Send(&currentRow, 1, MPI_INT, currentProcess, 0, MCW); 
-				currentProcess++;
-				if(currentProcess == size) {currentProcess = 1;}
-			}
-			currentProcess = 1;
-			for(int j=0;j<PIXELS;++j) {
-				rgbVect.resize(PIXELS*3);
-				MPI_Recv(&rgbVect[0], (PIXELS * 3), MPI_INT, currentProcess, j, MCW, MPI_STATUS_IGNORE);
-				currentProcess++;
-				if(currentProcess == size) {currentProcess = 1;}
-				int index = 0;
-				for(int k = 0; k < PIXELS; ++k) {
-					for(int l = 0; l < 3; l++) {
-						cout<<rgbVect[index]<<" ";
-						index++;
-					}
-				}
-				cout<<endl;
-			}
-	}
-	else {
-		MPI_Recv(&currentRow, 1, MPI_INT, 0, 0, MCW, MPI_STATUS_IGNORE);
-		rgbVect.clear();
-		for(int j=0;j<PIXELS;++j){
-		    cx.i = c1.i + j * iinc;
-		    cx.r = c1.r + currentRow * rinc;
-		    iters = mbrot_iters(cx);        
-		    getColor(iters, rgbVect);
-	  } 
-		cout<<"current row before send: "<<currentRow<<" Rank: "<<rank<<endl;;	
-		MPI_Send(&rgbVect[0], (PIXELS*3), MPI_INT, 0, currentRow, MCW);
-	}
 
+		currentRow = 0;
+		for(int i=1;i<size;++i){ //Send out initial rows to each process
+			MPI_Send(&currentRow, 1, MPI_INT, i, 0, MCW); 
+			currentRow++;
+		}
+
+		int received = 0;
+		while(received < PIXELS - 1) { //Continue while fewer than PIXELS rows have been received
+			MPI_Status status;
+			rgbVect.resize(PIXELS*3);
+			//Receive a vector of RGB values
+			t
+			t
+			t
+			t
+			t
+			t
+			t
+			MPI_Recv(&rgbVect[0], (PIXELS * 3), MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, &status);
+
+			for(int k = 0; k < (PIXELS * 3); k++) { //Copy vector RGB values over to the main array
+				mainArray[status.MPI_TAG][k] = rgbVect[k];
+			}
+
+			received++;
+			if(currentRow < PIXELS - 1) { 
+				//If not all rows have been sent out, send the next one to the process that just finished
+				currentRow++;
+				MPI_Send(&currentRow, 1, MPI_INT, status.MPI_SOURCE, 0, MCW);
+			}
+		}
 		
+		for(int i = 1; i < size; i++) { //Send -1 to each process to signify that the work is done
+			currentRow = -1;
+			MPI_Send(&currentRow, 1, MPI_INT, i, 0, MCW);
+		}
+
+		for(int i = 0; i < PIXELS; i++) { //Print out all values to the PPM file
+			for(int j = 0; j < PIXELS * 3; ++j) {
+				cout<<mainArray[i][j]<<" ";
+			}
+			cout<<endl;
+		}	
+	}
+	else { //If I am not the master
+		currentRow = 0;
+		while(currentRow != -1) {
+			MPI_Recv(&currentRow, 1, MPI_INT, 0, 0, MCW, MPI_STATUS_IGNORE); //Receive a row to work on
+			
+			rgbVect.clear();
+			if(currentRow != -1) { //If the currentRow is not the stop signal, continue
+				for(int j=0;j<PIXELS;++j){
+						cx.i = c1.i + j * iinc;
+						cx.r = c1.r + currentRow * rinc;
+						iters = mbrot_iters(cx);        
+						getColor(iters, rgbVect); //Fill the vector with RGB values
+				} 
+				MPI_Send(&rgbVect[0], (PIXELS*3), MPI_INT, 0, currentRow, MCW); //Send my result back to master
+			}
+		}
+	}
+	
 	MPI_Finalize();
 
 	return 0;
